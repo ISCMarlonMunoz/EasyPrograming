804b5a32137ddd32403f6e51f36628b573f1f294
Marlon Muñoz <Marlon Muoz@DESKTOP-4TJLGGT>
2019-12-03 15:56:31 -0600

    Agregar archivos de proyecto.

---
+++ EasyPrograming/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EasyPrograming.Areas.Identity.Data;
+using EasyProgramingCodex.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EasyPrograming.Areas.Identity.IdentityHostingStartup))]
+namespace EasyPrograming.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EasyProgramingContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EasyProgramingContextConnection")));
+
+                
+            });
+        }
+    }
+}
